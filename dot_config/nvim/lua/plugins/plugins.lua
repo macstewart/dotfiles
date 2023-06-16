c22ade65931ab65ac108e6175196826c715965b2
mac <mac@unix.localdomain>
2023-06-16 17:07:54 -0400

    nvim: plugin file formatting updates

--- dot_config/nvim/lua/plugins/plugins.lua
+++ dot_config/nvim/lua/plugins/plugins.lua
@@ -43,15 +43,15 @@ packer.init({
 
 -- Install your plugins here
 return packer.startup(function(use)
-    use({ "wbthomason/packer.nvim" }) -- Have packer manage itself
-    use({ "nvim-lua/plenary.nvim" }) -- Useful lua functions used by lots of plugins
-    use({ "windwp/nvim-autopairs" }) -- Autopairs integrates with both cmp and treesitter
-    use({ "numToStr/Comment.nvim" }) -- adds comments textobjects and hotkeys
-    use({ "JoosepAlviste/nvim-ts-context-commentstring" }) --adds support for context sensitive comment blocks in certain languages
-    use({ "nvim-tree/nvim-web-devicons" }) --devicons
+    use({ "wbthomason/packer.nvim" })                                                       -- Have packer manage itself
+    use({ "nvim-lua/plenary.nvim" })                                                        -- Useful lua functions used by lots of plugins
+    use({ "windwp/nvim-autopairs" })                                                        -- Autopairs integrates with both cmp and treesitter
+    use({ "numToStr/Comment.nvim" })                                                        -- adds comments textobjects and hotkeys
+    use({ "JoosepAlviste/nvim-ts-context-commentstring" })                                  --adds support for context sensitive comment blocks in certain languages
+    use({ "nvim-tree/nvim-web-devicons" })                                                  --devicons
     use({ "nvim-tree/nvim-tree.lua", commit = "9c97e6449b0b0269bd44e1fd4857184dfa57bb4c" }) --file manager/file bar
-    use({ "moll/vim-bbye" }) --better buffer close behavior
-    use({ "nvim-lualine/lualine.nvim" }) --bottom status bar
+    use({ "moll/vim-bbye" })                                                                --better buffer close behavior
+    use({ "nvim-lualine/lualine.nvim" })                                                    --bottom status bar
     use({ "akinsho/toggleterm.nvim" })
     use({ "ahmedkhalf/project.nvim" })
     use({ "lewis6991/impatient.nvim" })
@@ -72,22 +72,22 @@ return packer.startup(function(use)
     use({ "norcalli/nvim-colorizer.lua" })
 
     -- cmp plugins
-    use({ "hrsh7th/nvim-cmp" }) -- The completion plugin
-    use({ "hrsh7th/cmp-buffer" }) -- buffer completions
-    use({ "hrsh7th/cmp-path" }) -- path completions
+    use({ "hrsh7th/nvim-cmp" })         -- The completion plugin
+    use({ "hrsh7th/cmp-buffer" })       -- buffer completions
+    use({ "hrsh7th/cmp-path" })         -- path completions
     use({ "saadparwaiz1/cmp_luasnip" }) -- snippet completions
     use({ "hrsh7th/cmp-nvim-lsp" })
     use({ "hrsh7th/cmp-nvim-lua" })
     use({ "akinsho/bufferline.nvim" })
     use({ "weilbith/nvim-code-action-menu", cmd = "CodeActionMenu" })
     -- snippets
-    use({ "L3MON4D3/LuaSnip" }) --snippet engine
+    use({ "L3MON4D3/LuaSnip" })             --snippet engine
     use({ "rafamadriz/friendly-snippets" }) -- a bunch of snippets to use
 
     -- LSP
     -- use { "williamboman/nvim-lsp-installer", commit = "e9f13d7acaa60aff91c58b923002228668c8c9e6" } -- simple to use language server installer
     use("ray-x/go.nvim")
-    use("ray-x/guihua.lua") -- recommended if need floating window support
+    use("ray-x/guihua.lua")          -- recommended if need floating window support
     use({ "neovim/nvim-lspconfig" }) -- enable LSP
     use({ "williamboman/mason.nvim" })
     use({ "williamboman/mason-lspconfig.nvim" })
@@ -129,6 +129,17 @@ return packer.startup(function(use)
                 require("plugins.copilot-cmp")
             end,
         }),
+    })
+    use({
+        "jackMort/ChatGPT.nvim",
+        config = function()
+            require("chatgpt").setup()
+        end,
+        requires = {
+            "MunifTanjim/nui.nvim",
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim"
+        }
     })
     -- DAP
     use({ "mfussenegger/nvim-dap" })
