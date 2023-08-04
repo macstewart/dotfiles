384320fb820b6522969bdab7aa660601659104e0
Mackenzie Stewart <github@macstew.art>
2023-08-04 09:52:32 -0400

    wezterm: minor style updates

--- dot_config/wezterm/style.lua
+++ dot_config/wezterm/style.lua
@@ -3,13 +3,13 @@ require("util/statusline/segment")
 require("util/color")
 local sep = ""
 local rev_sep = ""
-local scheme = wezterm.get_builtin_color_schemes()["Catppuccin Mocha"]
+local scheme = wezterm.get_builtin_color_schemes()["Catppuccin Latte"]
 
 StyleConfig = {
 	font = wezterm.font("FiraCode Nerd Font", { weight = "Medium", stretch = "Normal", style = "Normal" }),
 	font_size = 13.0,
 
-	color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte
+	color_scheme = "Tokyo Night (Gogh)", -- or Macchiato, Frappe, Latte
 	use_fancy_tab_bar = false,
 	-- inactive_pane_hsb = { saturation = 0.6, brightness = 0.4 },
 	show_new_tab_button_in_tab_bar = false,
