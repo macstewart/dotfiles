6c45b8d6168be016fb883c6d1cbac17d3896daab
Mackenzie Stewart <github@macstew.art>
2023-09-08 12:19:43 -0400

    nnn: bigger preview split

--- dot_config/zsh/_export/nnn.zsh
+++ dot_config/zsh/_export/nnn.zsh
@@ -3,3 +3,4 @@ _dotfileLog "sourcing nnn.zsh" 2>/dev/null
 export NNN_PLUG="f:finder;o:fzopen;p:preview-tui;d:diffs;t:transfer;i:preview-tabbed;s:xcc"
 
 export NNN_FIFO=/tmp/nnn.fifo
+export NNN_SPLITSIZE=75
