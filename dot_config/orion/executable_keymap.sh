9834bbcb55336e3764cd173dd75634f94759a6bd
Mackenzie Stewart <github@macstew.art>
2023-09-23 12:20:46 -0400

    native keymapping tests

---
+++ dot_config/orion/executable_keymap.sh
@@ -0,0 +1,9 @@
+#/usr/bin/env bash
+
+defaults write com.kagi.kagimacOS NSUserKeyEquivalents '{
+  "New Tab"="t";
+  "Close Tab"="x";
+  "Open Location..."="l";
+  "Show Next Tab"="$l";
+  "Show Previous Tab"="$h";
+}'
