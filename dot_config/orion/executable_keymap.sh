535c5cdc5df9aae381ab8b27ac71ed0ac9a5d434
Mackenzie Stewart <github@macstew.art>
2023-10-11 09:01:33 -0400

    macos: add svim and update orion keymap

--- dot_config/orion/executable_keymap.sh
+++ dot_config/orion/executable_keymap.sh
@@ -3,7 +3,14 @@
 defaults write com.kagi.kagimacOS NSUserKeyEquivalents '{
   "New Tab"="t";
   "Close Tab"="x";
+  "Undo Tab Close"="u";
+
   "Open Location..."="l";
   "Show Next Tab"="$l";
   "Show Previous Tab"="$h";
+
+  "Forward"="^i";
+  "Back"="^o";  
+  "Show Sidebar"="\\";
+  "Hide Sidebar"="\\";
 }'
