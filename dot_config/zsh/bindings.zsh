43b1a8c73eba43db2428b7a4ddaf627077515069
Mackenzie Stewart <github@macstew.art>
2023-11-12 11:54:50 -0500

    zsh: jq repl binding tweaks

--- dot_config/zsh/bindings.zsh
+++ dot_config/zsh/bindings.zsh
@@ -2,7 +2,7 @@ zle -N edit-command-line
 
 bindkey -r '^J'
 
-bindkey "^v" jq-complete
+bindkey "^t" jq-complete
 
 bindkey -M vicmd v edit-command-line
 
--- dot_config/zsh/functions.zsh
+++ dot_config/zsh/functions.zsh
@@ -15,25 +15,25 @@ function listutils() {
     ls ~/utils
 }
 
-function jq-repl () {
-  local query_file data_file new_pane_height nodemon_cmd change_aucmd jq_args
-
-  # store input in file
-  query_file=$(mktemp)
-  data_file=${query_file}.json
-  cat /dev/stdin > $data_file
-  new_pane_height=5
-
-  # pass args to jq
-  jq_args="$@ -C -f $query_file $data_file" 
-  nodemon_cmd="clear; jq $jq_args"
-
-  # inject autocommand into vim, saving file on each edit, triggering nodemon, which 
-  # runs jq the corresponding jq command
-  change_aucmd="au TextChanged,TextChangedI <buffer> write"
-  tmux split-window -l$new_pane_height vim -c "$change_aucmd" -c "set ft=jq" $query_file
-  nodemon -q -d 0.3 -w $query_file -x "$nodemon_cmd"
-}
+# function jq-repl () {
+#   local query_file data_file new_pane_height nodemon_cmd change_aucmd jq_args
+#
+#   # store input in file
+#   query_file=$(mktemp)
+#   data_file=${query_file}.json
+#   cat /dev/stdin > $data_file
+#   new_pane_height=5
+#
+#   # pass args to jq
+#   jq_args="$@ -C -f $query_file $data_file" 
+#   nodemon_cmd="clear; jq $jq_args"
+#
+#   # inject autocommand into vim, saving file on each edit, triggering nodemon, which 
+#   # runs jq the corresponding jq command
+#   change_aucmd="au TextChanged,TextChangedI <buffer> write"
+#   tmux split-window -l$new_pane_height vim -c "$change_aucmd" -c "set ft=jq" $query_file
+#   nodemon -q -d 0.3 -w $query_file -x "$nodemon_cmd"
+# }
 
 function re() {
     if [ $# -gt 0 ]; then # assume arg is debug flag, TODO don't do this
