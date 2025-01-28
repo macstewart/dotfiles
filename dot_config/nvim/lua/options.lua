2c90638c9e66c6bdc663b018cc65e10c54eee4d3
Mackenzie Stewart <github@macstew.art>
2025-01-28 16:33:38 -0500

    working avante config

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -10,6 +10,8 @@ api.nvim_set_var("tmux_navigator_save_on_switch", 1)
 api.nvim_set_var("tmux_navigator_no_wrap", 1)
 api.nvim_set_var("tmux_navigator_disable_netrw_workaround", 1)
 
+vim.opt.laststatus = 3
+
 api.nvim_create_autocmd("FileType", {
   group = vim.api.nvim_create_augroup("FixSqlCommentstring", { clear = true }),
   callback = function(ev)
